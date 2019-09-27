5db61f0fde3f526c3a74bcdf6943b5d0f539eea0
Sean Leif Hellier <sean@newtropolis.com>
2019-09-27 16:45:33 -0400

    created file structure

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-	id: process.env.SPOTIFY_ID,
-	secret: process.env.SPOTIFY_SECRET
+	id: process.env.b2200fd28aa34c70ad778ac3ded5b8ae,
+	secret: process.env.09372369309942e4a937fb2bca933be7
 };
---
+++ liri.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
